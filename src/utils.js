14137aa6bff9078d704a170ee4d454f847d3be76
Dan <27021931+DanWaLes@users.noreply.github.com>
2026-06-01 16:35:51 +0000

    Create utils.js
    partly automates the process

---
+++ src/utils.js
@@ -0,0 +1,16 @@
+function encodedToRule(str) {
+	// can be used on:
+	// * images.saatchiart.com by-user
+	// * wixmp.com CDN by-user
+	// shouldnt be used on:
+	// * artlimited.net by-user
+
+	// remove last character
+	str = str.substring(0, str.length - 1);
+
+	// insert slashes every 16 characters
+	str = str.replace(/.{16}/g, '$&/');
+
+	// add the rule
+	return `search.brave.com##.image-result:has(img[src^="https://imgs.search.brave.com"][src*="/${str}"])`;
+}
